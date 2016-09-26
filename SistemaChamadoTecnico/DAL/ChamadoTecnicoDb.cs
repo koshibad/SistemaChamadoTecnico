f003b3d12f40e23c9195e8cd8f342f5f9d2ecf87
Diogo Koshiba <diogo.koshiba@gmail.com>
2016-09-25 23:32:40 -0300

    home + controllers

---
+++ SistemaChamadoTecnico/DAL/ChamadoTecnicoDb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaChamadoTecnico.DAL
+{
+    public class ChamadoTecnicoDb
+    {
+    }
+}
